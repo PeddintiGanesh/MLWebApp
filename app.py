7ce084cb22a5bc6dac2caec47cff36be23c2b3bd
Peddinti Ganesh <157904191+PeddintiGanesh@users.noreply.github.com>
2024-11-01 14:46:36 +0530

    app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-heart_disease_model = pickle.load(open("heart_disease_model.sav", 'rb'))
-
+heart_disease_model = pickle.load(open("heart_disease_model.sav", "rb"))
 
 # sidebar for navigation
 with st.sidebar:
